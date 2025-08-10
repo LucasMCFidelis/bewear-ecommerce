6f6017e1d43d60e5ebc69ca6354bfa290e88ee47
Lucas Fidelis <lucasm241301@gmail.com>
2025-08-10 15:47:55 -0300

    style: set display flex to body and flex-1 to wrapper children

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,13 +28,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
         <Header />
-        {children}
-        <Toaster />
+        <div className="flex-1">
+          {children}
+          <Toaster />
+        </div>
         <Footer />
       </body>
     </html>
